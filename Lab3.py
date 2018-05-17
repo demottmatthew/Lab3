d7958846ba93e37274b7fea32aff473a5afa9ce9
Matthew DeMott <demottmatthew@gmail.com>
2018-05-17 15:09:13 -0400

    adding my fibonacci method

--- Lab3.py
+++ Lab3.py
@@ -4,7 +4,23 @@ def power(n, p):
     else:
         return n * power(n, p -1)
 
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n -1) + fib(n-2)
+
 def main():
-    print(power(2, 3))
+    #print(power(2, 3))
+    print(fib(0))
+    print(fib(1))
+    print(fib(2))
+    print(fib(3))
+    print(fib(4))
+    print(fib(5))
+    print(fib(6))
+    print(fib(7))
 
 main()
