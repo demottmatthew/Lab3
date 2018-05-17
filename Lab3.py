b2f58d120604e603deb500e42b0e2de410e596ca
Matthew DeMott <demottmatthew@gmail.com>
2018-05-17 14:57:03 -0400

    adding the power function

--- Lab3.py
+++ Lab3.py
@@ -1,11 +1,10 @@
-def factorial(n): #define our function with parameter n which is our integer that we want to multiply
-    if n == 0: #if we get n equal to 0 then we return 1 because 0! is equal to 1
+def power(n, p):
+    if p == 0:
         return 1
-    else: #if not 0 then we want to multiply our current number times our next n
-            #for example if n = 4 then we are doing 4 * 3
-        return n * factorial(n-1)
+    else:
+        return n * power(n, p -1)
 
 def main():
-    print(factorial(4))
+    print(power(2, 3))
 
 main()
